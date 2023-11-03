bdc3bd6699799c4460b525ef518b8aa977ad6d4d
NiallDionne <144242112+NiallDionne@users.noreply.github.com>
2023-11-03 09:00:23 -0400

    Made menu

--- assignment.py
+++ assignment.py
@@ -1,1 +1,8 @@
-
+import math
+print ("Freddy Fazbear's pizzeria")
+print ("Large pizza $6.00")
+print ("Extra large pizza $10.00")
+Print ("1 topping $1.00")
+print ("2 toppings $1.75")
+print ("3 toppings $2.50")
+print ("4 toppings $3.35")
